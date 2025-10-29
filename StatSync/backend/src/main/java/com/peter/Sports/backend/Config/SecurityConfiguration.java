83e42d131aea04532ada64ab4deabf77344b8cb0
Peter Tran <petertrann24@gmail.com>
2025-10-28 20:34:39 -0400

    cors path

--- StatSync/backend/src/main/java/com/peter/Sports/backend/Config/SecurityConfiguration.java
+++ StatSync/backend/src/main/java/com/peter/Sports/backend/Config/SecurityConfiguration.java
@@ -72,12 +72,15 @@ public class SecurityConfiguration {
     @Bean 
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080", 
-        "https://*.vercel.app"));
+        configuration.setAllowedOriginPatterns(List.of(
+            "http://localhost:5173",
+            "http://localhost:8080", 
+            "https://*.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
-
+    
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
