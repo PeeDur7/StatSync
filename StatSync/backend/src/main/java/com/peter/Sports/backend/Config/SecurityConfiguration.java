7849b0374fb9e915dccb425d018bab0f63bd84a5
Peter Tran <petertrann24@gmail.com>
2025-10-28 20:28:58 -0400

    cors

--- StatSync/backend/src/main/java/com/peter/Sports/backend/Config/SecurityConfiguration.java
+++ StatSync/backend/src/main/java/com/peter/Sports/backend/Config/SecurityConfiguration.java
@@ -73,8 +73,7 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:8080", 
-        "https://stat-sync-deploy-5ni31i3pi-peedur7s-projects.vercel.app",
-        "https://stat-sync-deploy-5ni31i3pi-peedur7s-projects.vercel.app"));
+        "https://*.vercel.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
         configuration.setAllowCredentials(true);
