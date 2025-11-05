29a7f8ebbc4ba80043a68e11d59ac60d6aa222e2
Petertrann7 <petertrann7@vt.edu>
2025-11-05 18:20:05 -0500

    fix nfl nav

--- StatSync/frontend/src/Components/NFLNavBar.jsx
+++ StatSync/frontend/src/Components/NFLNavBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function NFLNavBar({selectedPosition, setSelectedPosition,
     filterText, setFilterText, loadPlayers,
